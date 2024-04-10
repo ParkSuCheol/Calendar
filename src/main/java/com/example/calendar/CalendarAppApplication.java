fcacf010c4184a58f0bbaa15fa509cd2c094044d
parksucheol <3864617@naver.com>
2024-04-10 23:37:31 +0900

    Feat. csv파일 Read 기능 추가, opencsv api 추가

--- src/main/java/com/example/calendar/CalendarAppApplication.java
+++ src/main/java/com/example/calendar/CalendarAppApplication.java
@@ -6,6 +6,8 @@ import com.example.calendar.event.EventType;
 import com.example.calendar.event.Meeting;
 import com.example.calendar.event.Schedule;
 import com.example.calendar.event.Todo;
+import com.example.calendar.reader.EventCsvReader;
+import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -13,35 +15,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CalendarAppApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Schedule schedule = new Schedule();
 
-        HashSet<String> participants = new HashSet<>();
-        participants.add("danny.kim");
+        EventCsvReader csvReader = new EventCsvReader();
+        String meetingCsvPath = "/data/meeting.csv";
 
-        Meeting meeting1 = new Meeting(
-                1, "meeting1",
-                ZonedDateTime.now(), ZonedDateTime.now().plusHours(1),
-                participants,"meetingRoomA", "스터디"
-        );
+        List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+        meetings.forEach(each -> schedule.add(each));
 
-        schedule.add(meeting1);
-
-        Todo todo1 = new Todo(
-                2, "todo1",
-                ZonedDateTime.now().plusHours(3), ZonedDateTime.now().plusHours(4),
-                "할 일 적기"
-        );
-
-        Todo todo2 = new Todo(
-                3, "todo2",
-                ZonedDateTime.now().plusHours(6), ZonedDateTime.now().plusHours(8),
-                "할 일 적기"
-        );
-
-        schedule.add(todo1);
-
-        //schedule.printBy(EventType.TO_DO);
         schedule.printAll();
     }
 }
